ce2874332ecc1e8570ac248893d94fcadd61cce4
Chen yaosheng <20182333002@m.scnu.edu.cn>
2024-06-18 15:22:47 +0800

    来单提醒功能代码开发

--- sky-server/src/main/java/com/sky/mapper/OrderDetailMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderDetailMapper.java
@@ -2,6 +2,8 @@ package com.sky.mapper;
 
 import com.sky.entity.OrderDetail;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -12,4 +14,12 @@ public interface OrderDetailMapper {
      * @param orderDetailList
      */
     public void insertBatch(List<OrderDetail> orderDetailList);
+
+    /**
+     * 查询订单明细
+     * @param orderId
+     * @return
+     */
+    @Select("select * from order_detail where order_id = #{orderId}")
+    List<OrderDetail> getByOrderId(Long orderId);
 }
--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -1,7 +1,14 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.Orders;
+import io.swagger.models.auth.In;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Mapper
 public interface OrderMapper {
@@ -12,4 +19,35 @@ public interface OrderMapper {
      */
     void insert(Orders orders);
 
+    /**
+     * 分页条件查询
+     * @param ordersPageQueryDTO
+     * @return
+     */
+    Page<Orders> pageQuery(OrdersPageQueryDTO ordersPageQueryDTO);
+
+    /**
+     * 根据id查询订单
+     * @param id
+     * @return
+     */
+    @Select("select * from orders where id=#{id}")
+    Orders getByid(Long id);
+
+    /**
+     * 更新订单状态
+     * @param orders
+     */
+    void update(Orders orders);
+
+    /**
+     * 根据状态，分别查询出待接单、待派送、派送中的订单数量
+     * @param toBeConfirmed
+     * @return
+     */
+    @Select("select count(id) from orders where status = #{status}")
+    Integer countStatus(Integer toBeConfirmed);
+
+    @Select("select * from orders where status = #{status} and order_time < #{orderTime}")
+    List<Orders> getByStatusAndOrderTimeLT(Integer status, LocalDateTime orderTime);
 }
